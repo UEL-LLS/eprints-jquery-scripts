46f8999a31b338a1f4492a47d4095017a8ea25c5
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-11 16:02:35 +0100

    navbar menu added

---
+++ navbar-menu.js
@@ -0,0 +1,15 @@
+/**************************
+ Script to change eprint  
+ login menu to bootstap 
+ nav-bar 
+***************************/
+var j = jQuery.noConflict();
+
+j(document).ready(function () {
+
+	//left hand side menu. Addes the classes needed for bootstrap to style the menu 
+	j('#ep_tm_menu_tools, .ep_tm_key_tools').addClass('nav navbar-nav');
+
+	j('#ep_tm_menu_tools li:first').wrapInner('<a href="#" style="cursor:default" />');
+});
+
