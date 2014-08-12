50db14fb34f020ca11f5959a4221233f1636b9b7
David McElroy <d.mcelroy@uel.ac.uk>
2014-08-12 13:42:33 +0100

    roar-addnew.js  should mimic the roar implimentation of the add new button(enventually)

---
+++ roar-addnew.js
@@ -0,0 +1,47 @@
+/**************************
+ Script to add roar-style
+ add new button
+***************************/
+var j = jQuery.noConflict();
+
+//button position
+var fromtop = "1em";
+var fromright = "1em";
+
+j(document).ready(function () {
+
+
+		j('body').append('<div id="add_item"></div>');
+		j('<button />', {
+			id: "add_item_show",
+			type: "button",
+			class: "btn btn-lg btn-docklands-prime pull_right",
+			title: "Add a new item"
+		}).appendTo('#add_item');
+		j('#add_item').append('<div id="add_item"></div>');
+		
+			
+	
+		//create div with code in it here
+		j('#add_item_content').append('<form method="post" accept-charset="utf-8" action="/cgi/users/home" enctype="multipart/form-data"></form>');
+		j('<input/>', {
+			name: "screen",
+			id: "screen",
+			value: "NewEPrint",
+			type: "hidden"
+		}).appendTo('#addnewitem form');
+		j('<input/>', {
+			value: "New Publication",
+			name: "_action_create",
+			type: "submit",
+			class: "btn btn-primary btn-docklands-prime"
+		}).appendTo('#addnewitem form');
+		j('#addnewitem').css({
+			'position': 'absolute',
+			'top': fromtop,
+			'right': fromright
+		});
+
+
+
+});
