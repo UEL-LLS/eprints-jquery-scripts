95b65fe7aa9feb9437a5ba75aeb92111ef303aab
David McElroy <d.mcelroy@uel.ac.uk>
2014-10-28 15:37:46 +0000

    created altmetric btn thing

---
+++ altmetic-btn.js
@@ -0,0 +1,11 @@
+/**************************
+ Blank Script
+
+***************************/
+var j = jQuery.noConflict();
+
+j(document).ready(function () {
+
+	j('.altmetric_details').addClass('btn btn-docklands-prime pull-right').removeClass('altmetric_details');
+
+});
