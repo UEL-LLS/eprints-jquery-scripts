b37cefad3f1d0d38ca966e1c50f8677ee1732773
David McElroy <d.mcelroy@uel.ac.uk>
2014-10-28 14:31:44 +0000

    switched to colour variables

--- projector.js
+++ projector.js
@@ -4,20 +4,23 @@
 ***************************/
 var j = jQuery.noConflict();
 
+var colour1 = "#8B00A3";
+var colour2 = "#700084";
+
 j(document).ready(function () {
 
 	if (j("meta[name='eprints.type']").attr("content") == "collection"){ 
-		j('#main_title h1').attr('style', 'background-color: #B800D8!important');
-		j('.btn-docklands-prime').attr('style', 'background-color: #B800D8!important');
+		j('#main_title h1').attr('style', 'background-color: '+colour1+'!important');
+		j('.btn-docklands-prime').attr('style', 'background-color: '+colour1+'!important');
 		j('.btn-docklands-prime').mouseover(function(){
-			j(this).attr('style', 'background-color: red!important');
+			j(this).attr('style', 'background-color: '+colour2+'!important');
 		});
 		j('.btn-docklands-prime').mouseout(function(){
-                        j(this).attr('style', 'background-color: #B800D8!important');
+                        j(this).attr('style', 'background-color: '+colour1+'!important');
                 });     
-		j('#summary_downloads .panel-docklands-prime .panel-heading').attr('style', 'background-color: #B800D8!important');
+		j('#summary_downloads .panel-docklands-prime .panel-heading').attr('style', 'background-color: '+colour1+'!important');
 
-		j('.docklands_footer').attr('style', 'border-color: #B800D8!important');
+		j('.docklands_footer').attr('style', 'border-color: '+colour2+'!important');
 	};
 
 });
