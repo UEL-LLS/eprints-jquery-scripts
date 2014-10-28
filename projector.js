81c112a6b89b1083b21817943ea9981a59b80a24
David McElroy <d.mcelroy@uel.ac.uk>
2014-10-28 14:19:09 +0000

    make projects in data.uel purple. might be too much colour though...

---
+++ projector.js
@@ -0,0 +1,21 @@
+/**************************
+ Blank Script
+
+***************************/
+var j = jQuery.noConflict();
+
+j(document).ready(function () {
+
+	if (j("meta[name='eprints.type']").attr("content") == "collection"){ 
+		j('#main_title h1').attr('style', 'background-color: #B800D8!important');
+		j('.btn-docklands-prime').attr('style', 'background-color: #B800D8!important');
+		j('.btn-docklands-prime').mouseover(function(){
+			j(this).attr('style', 'background-color: red!important');
+		});
+		j('.btn-docklands-prime').mouseout(function(){
+                        j(this).attr('style', 'background-color: #B800D8!important');
+                });     
+		j('#summary_downloads) .panel-docklands-prime .panel-heading').attr('style', 'background-color: #B800D8!important');
+	};
+
+});
