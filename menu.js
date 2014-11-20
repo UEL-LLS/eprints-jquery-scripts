78dafa6a8947d00f4ce1a06ec03e9c8680ee6138
David McElroy <d.mcelroy@uel.ac.uk>
2014-11-20 16:50:11 +0000

    update to the menu stuff to allow menu to be embeded in the navbar on mobile view

--- menu.js
+++ menu.js
@@ -10,8 +10,11 @@ j(document).ready(function () {
 	//left hand side menu. Addes the classes needed for bootstrap to style the menu 
 	j('#ep_tm_menu_tools, .ep_tm_key_tools').addClass('nav nav-pills nav-stacked');
 	//menu manipluation and collapse stuff
-	j('.ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('navbar-collapse collapse').attr('id','ep_menu');
-	
+	//if on side menu {
+	j('#main_page .ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('navbar-collapse collapse').attr('id','ep_menu');
+	//}else {
+	j('header .ep_tm_key_tools').removeClass('ep_tm_key_tools').addClass('navbar-collapse collapse').attr('id','ep_menu');
+	//}
 	j('nav #ep_menu li').wrapInner('<a style="cursor:default;color:#555 " href="#"></a>');
 
 });
